aa30b8e29eea25b38a7575f66bbd5b5745e8c7bf
Gauravkumar4460 <gauravkr194460@gmail.com>
2024-07-14 08:27:35 +0530

    Local storage facility added

--- src/ProjectManager.js
+++ src/ProjectManager.js
@@ -1,31 +1,74 @@
 // projectManager.js
 
-import { Project } from './Todo.js';
+import { Project,Todo } from './Todo.js';
 
 class ProjectManager {
     constructor() {
-        this.projects = [];
+        this.projects = this.loadFromLocalStorage() || [];
         this.defaultProject = new Project('Project 1');
-        this.projects.push(this.defaultProject);
-        this.currentProject = this.defaultProject;
+
+        if(this.projects.length === 0){
+            this.projects.push(this.defaultProject);
+        }
+
+        this.currentProjectIndex = this.loadCurrentProjectIndexFromLocalStorage() || 0;
+        this.currentProject = this.projects[this.currentProjectIndex];
     }
 
     addProject(project) {
         this.projects.push(project);
+        this.saveToLocalStorage();
     }
 
     removeProject(index) {
         this.projects.splice(index, 1);
+        this.saveToLocalStorage();
     }
 
     setCurrentProject(index) {
-        this.currentProject = this.projects[index];
+        this.currentProjectIndex = index;
+        this.saveCurrentProjectIndexToLocalStorage();
+        this.currentProject = this.projects[this.currentProjectIndex];
     }
 
     getCurrentProject() {
         return this.currentProject;
     }
+
+    saveToLocalStorage() {
+        localStorage.setItem('projects',JSON.stringify(this.projects));
+    }
+    loadFromLocalStorage() {
+        const projectsJson = localStorage.getItem('projects');
+        if(projectsJson){
+            const projectsData = JSON.parse(projectsJson);
+            return projectsData.map(project => {
+                const newProject = new Project(project.name);
+                project.todos.forEach(todo => {
+                        newProject.addTodo(new Todo(
+                            todo.title,
+                            todo.description,
+                            todo.dueDate,
+                            todo.priority,
+                            todo.notes,
+                            todo.checklist,
+                            todo.completed = false
+                        ));                 
+                });
+                return newProject;
+            });
+        }
+        return null;
+    }
+
+    saveCurrentProjectIndexToLocalStorage() {
+        localStorage.setItem('currentProjectIndex',this.currentProjectIndex);
+    }
+    loadCurrentProjectIndexFromLocalStorage() {
+        const index = localStorage.getItem('currentProjectIndex');
+        return index ? parseInt(index) : 0;
+    }
 }
 
-// Export the ProjectManager class for use in other modules
+
 export { ProjectManager };
