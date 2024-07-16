f38b5264a5abaa74dc8160d8184599f173cae116
Gauravkumar4460 <gauravkr194460@gmail.com>
2024-07-16 10:25:53 +0530

    bable is added to project

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -19,6 +19,13 @@ export default {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
     ],
   },
   plugins: [
